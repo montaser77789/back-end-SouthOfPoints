f9d2ca416b6070372b0112b23e9929292fb18ff0
montaser <montasergohar@gmail.com>
2025-03-27 09:21:27 +0200

    Vercel

--- app.js
+++ app.js
@@ -16,12 +16,19 @@ app.use(cookieParser());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-    origin: "https://south-zone-points.vercel.app", // اسمح فقط للفرونت إند الخاص بك
+    origin: "https://south-zone-points.vercel.app", // فرونت إند المسموح له
     methods: "GET,POST,PUT,DELETE",
-    credentials: true // ضروري إذا كنت تستخدم الكوكيز
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true
   }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://south-zone-points.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
   
-
 const userRouter = require('./routers/user_router');
 
 
