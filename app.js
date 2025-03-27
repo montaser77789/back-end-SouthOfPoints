a486997879186806df8871c915cf75a5bacf4697
montaser <montasergohar@gmail.com>
2025-03-27 10:48:30 +0200

    Vercel

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://south-zone-points.vercel.app"],
+    credentials: true
+  }));
+  
 
 const userRouter = require('./routers/user_router');
 
